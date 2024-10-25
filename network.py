408fa62c6d6c20950a19694c070fa0b94cb50225
wliu190 <wliu190@outlook.com>
2024-10-25 09:45:42 +0100

    Finish copying network.py from Nielsen

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class Network(object):
 
@@ -7,39 +8,86 @@ class Network(object):
         self.sizes = sizes
         self.biases = [np.random.randn(y,1) for y in sizes[1:]] # dimension n x 1, excludes first (input) layer 
         self.weights = [np.random.randn(y,x) for x, y in zip(sizes[:-1], sizes[1:])] # matrix of connections, w_ij with j -> i
-                                                                
+
+    def feedforward(self,a):
+        """Return the output of the network given 'a' as an input"""
+        for b,w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w,a)+b)
+        return a
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        """Trains the neutral network using minibatch stochastic gradient descent.
+        Training data is a list of tuples (x,y) containing training inputs and 
+        desired outputs"""
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [training_data[k:k+mini_batch_size] for k in range(0,n,mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch,eta)
+            if test_data:
+                print("Epoch {0}: {1} / {2}".format(j,self.evaluate(test_data),n_test))
+            else:
+                print("Epoch{0}complete".format(j))
+
+    def update_mini_batch(self, mini_batch, eta):
+        """Updates network weights and biases using SGD backpropagation to a single mini batch """
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x,y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x,y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+            self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+            self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop(self, x, y):
+        """
+        Returns tuple (nabla_b, nabla_w) which is the gradient for the cost function C_x
+        'nabla_b' and 'nabla_w' are layer-by-layer lists of numpy arrays
+        """
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        activation = x
+        activations = [x] 
+        zs = []
+        # feedforward
+        for b,w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].tranpose())
+        return (nabla_b, nabla_w)
+
+    def evaluate(self,test_data):
+        """
+        Returns the number of test inputs for which the neural network is correct
+        The neural network prediction is the output neuron with the highest activation
+        """
+        test_results = [(np.argmax(self.feedforward(x)), y) for (x,y) in test_data]
+        return sum(int(x == y) for (x,y) in test_results)
+    
+    def cost_derivative(self, output_activations, y):
+        """
+        Return the vector of partial derivatives for the output activations
+        """
+        return (output_activations - y)
+
+# Misc functions #
+##################
 def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
 
-def feedforward(self,a):
-    """Return the output of the network given 'a' as an input"""
-    for b,w in zip(self.biases, self.weights):
-        a = sigmoid(np.dot(w,a)+b)
-    return a
-
-def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
-    """Trains the neutral network using minibatch stochastic gradient descent.
-    Training data is a list of tuples (x,y) containing training inputs and 
-    desired outputs"""
-    if test_data: n_test = len(test_data)
-    n = len(training_data)
-    for j in range(epochs):
-        np.random.shuffle(training_data)
-        mini_batches = [training_data[k:k+mini_batch_size] for k in range(0,n,mini_batch_size)]
-        for mini_batch in mini_batches:
-            self.update_mini_batch(mini_batch,eta)
-        if test_data:
-            print("Epoch {0}: {1} / {2}".format(j,self.evaluate(test_data),n_test))
-        else:
-            print("Epoch{0}complete".format(j))
-
-def update_mini_batch(self, mini_batch, eta):
-    """Updates network weights and biases using SGD backpropagation to a single mini batch """
-    nabla_b = [np.zeros(b.shape) for b in self.biases]
-    nabla_w = [np.zeros(w.shape) for w in self.weights]
-    for x,y in mini_batch:
-        delta_nabla_b, delta_nabla_w = self.backprop(x,y)
-        nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-        nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
+def sigmoid_prime(z):
+    return sigmoid(z)*(1-sigmoid(z))
--- neural_network_one.py
+++ neural_network_one.py
@@ -2,3 +2,4 @@ import numpy as np
 import mnist_loader
 import network
 
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
